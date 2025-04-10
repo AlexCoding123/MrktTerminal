3dab6a86353886045a6ca65999fb891ed5fa9a2a
Alex Davila Alonso <alex.davilaalonso@dawsoncollege.qc.ca>
2025-04-10 19:40:30 -0400

    Update Project and include PugiXML

---
+++ build/CMakeFiles/MrktTerminal.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MrktTerminal.dir/MarketNews.cpp.o"
+  "CMakeFiles/MrktTerminal.dir/MarketNews.cpp.o.d"
+  "MrktTerminal"
+  "MrktTerminal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MrktTerminal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
